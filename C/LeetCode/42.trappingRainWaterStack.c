cf674677eb4da9f0663807a157003bf7af0c8801
Karansingh Bale <17437391+karanbale@users.noreply.github.com>
2021-07-08 14:32:43 -0700

    Update and rename 42.trappingRainWater.c to 42.trappingRainWaterStack.c

--- C/LeetCode/42.trappingRainWaterStack.c
+++ C/LeetCode/42.trappingRainWaterStack.c
@@ -1,28 +1,25 @@
 /*
 Given n non-negative integers representing an elevation map where,
 the width of each bar is 1, compute how much water it can trap after raining.
-
 Example 1:
 Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
 Output: 6
 Explanation: The above elevation map (black section) is represented by array,
 [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
-
 Example 2:
 Input: height = [4,2,0,3,2,5]
 Output: 9
  
-
 Constraints:
-
 n == height.length
 0 <= n <= 3 * 104
 0 <= height[i] <= 105
-
 */
 
 #include "../standardHeaders.h"
 
+#define ENABLE_PRINTS 0
+#define TM_PRINTF(f_, ...) ENABLE_PRINTS? printf((f_), ##__VA_ARGS__) : 0
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 typedef struct stackt{
     int *arr;
@@ -39,9 +36,9 @@ bool isStackFull(stackT *stack){
 }
 
 stackT *createStack(int stackSize){
-    stackT *stack = (stackT *) malloc(sizeof(stackT));
+    stackT *stack = malloc(sizeof(stackT));
     if(!stack)  return NULL;
-    stack->arr = (int *) malloc(sizeof(int)*stackSize);
+    stack->arr = malloc(sizeof(int)*stackSize);
     if(!stack->arr) return NULL;
     stack->front = -1;
     stack->stackSize = stackSize;
@@ -79,16 +76,22 @@ int trap(int* height, int heightSize){
     while (currentIdx < heightSize) {
         while(!isStackEmpty(stack) && (height[currentIdx] > height[stackPeek(stack)])){
             int previousTowerIdx = stackPop(stack);
-            printf("\tPopped height[%d]:%d from stack, currentIdx: %d\n",previousTowerIdx,height[previousTowerIdx], currentIdx);
+            TM_PRINTF("\tPopped height[%d]:%d from stack, currentIdx: %d\n",previousTowerIdx,height[previousTowerIdx], currentIdx);
             if(isStackEmpty(stack)) break;
             int waterLevel = MIN(height[currentIdx], height[stackPeek(stack)]) - height[previousTowerIdx];
             int distance = (currentIdx - stackPeek(stack) - 1);
             water += distance * waterLevel;
-            printf("\t\tDistance: %d, waterLevel: %d, water: %d\n",distance, waterLevel, water);
+            TM_PRINTF("\t\tDistance: %d, waterLevel: %d, water: %d\n",distance, waterLevel, water);
         }
         stackPush(stack, currentIdx);
-        printf("Pushing height[%d]:%d to stack\n",currentIdx,height[currentIdx]);
+        TM_PRINTF("Pushing height[%d]:%d to stack\n",currentIdx,height[currentIdx]);
         currentIdx++;
     }
     return water;
+}
+
+int main(void) {
+    int waterHeightArr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+    printf("Water level: %d\n", trap(&waterHeightArr,sizeof(waterHeightArr)/sizeof(waterHeightArr[0])));
+    return 0;
 }
