0c65332f71c0c01960d8a27c6502cfd2349c0846
Karansingh Bale <17437391+karanbale@users.noreply.github.com>
2021-07-08 14:29:07 -0700

    Update 42_TrappingRainWaterDP.c

--- C/DynamicProgramming/42_TrappingRainWaterDP.c
+++ C/DynamicProgramming/42_TrappingRainWaterDP.c
@@ -1,3 +1,21 @@
+/*
+Given n non-negative integers representing an elevation map where,
+the width of each bar is 1, compute how much water it can trap after raining.
+Example 1:
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array,
+[0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+Example 2:
+Input: height = [4,2,0,3,2,5]
+Output: 9
+ 
+Constraints:
+n == height.length
+0 <= n <= 3 * 104
+0 <= height[i] <= 105
+*/
+
 #include "../StandardHeaders.h"
 
 #define ENABLE_PRINTS 0
@@ -66,8 +84,14 @@ int trap(int* height, int heightSize){
     return ans;
 }
 
+void runAlgo(int *waterHeightArr, int size) {
+    printf("Water level: %d\n", trap(waterHeightArr, size));
+}
+
 int main(void) {
     int waterHeightArr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
-    printf("Water level: %d\n", trap(&waterHeightArr,sizeof(waterHeightArr)/sizeof(waterHeightArr[0])));
+    runAlgo(&waterHeightArr, sizeof(waterHeightArr)/sizeof(waterHeightArr[0]));
+    int waterHeightArr1[1] = {};
+    runAlgo(&waterHeightArr1, 1);
     return 0;
 }
